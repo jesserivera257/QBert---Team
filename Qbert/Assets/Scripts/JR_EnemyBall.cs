82e16e49257ed6d7cbc34c6c58fe2e1b9f43770c
jesserivera257 <33045560+jesserivera257@users.noreply.github.com>
2017-11-06 18:43:51 -0500

    1st

---
+++ Qbert/Assets/Scripts/JR_EnemyBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JR_EnemyBall : MonoBehaviour {
+
+	public GameObject myEnemy;
+	public Transform[] target;
+	public float speed;
+
+	private int current;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (transform.position != target [current].position) {
+
+			Vector3 pos = Vector3.MoveTowards (transform.position,target[current].position, speed*Time.deltaTime);
+			GetComponent<Rigidbody> ().MovePosition (pos);
+			//DestroyObject (myEnemy);
+		} else
+			current = (current + 1) % target.Length;
+		
+	}
+}
---
+++ Qbert/Assets/Scripts/JR_Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JR_Movement : MonoBehaviour {
+
+
+    public Transform leftCube;
+    public Transform rightCube;
+    public Transform topCube;
+	public Transform downCube;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.W))
+			transform.position += rightCube.position - downCube.position;
+        
+		if (Input.GetKeyDown (KeyCode.D))
+			transform.position += rightCube.position - topCube.position;
+
+		if (Input.GetKeyDown (KeyCode.A))
+			transform.position += leftCube.position - downCube.position;
+		
+		if (Input.GetKeyDown (KeyCode.S))
+			transform.position += leftCube.position - topCube.position;
+
+		if (Input.GetKeyDown (KeyCode.R))
+			SceneManager.LoadScene ("QBERT");
+
+
+		
+	}
+}
